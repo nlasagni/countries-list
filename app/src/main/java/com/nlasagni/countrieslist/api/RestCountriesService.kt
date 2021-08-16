c7b1bc432cb11a70d63107dd43fa0478eebe38ce
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-08-16 21:46:35 +0200

    Adds service for fetching all countries

---
+++ app/src/main/java/com/nlasagni/countrieslist/api/RestCountriesService.kt
@@ -0,0 +1,64 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Nicola Lasagni
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.nlasagni.countrieslist.api
+
+import com.nlasagni.countrieslist.data.Country
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+/**
+ * Used to connect to the RestCountries API to fetch countries.
+ *
+ * Created by Nicola Lasagni on 16/08/2021.
+ */
+interface RestCountriesService {
+
+    @GET("all")
+    suspend fun fetchAllCountries(): List<Country>
+
+    companion object {
+        private const val BASE_URL = "https://restcountries.eu/rest/v2/"
+
+        fun create(): RestCountriesService {
+            val logger = HttpLoggingInterceptor().apply { level = Level.BASIC }
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(RestCountriesService::class.java)
+        }
+
+    }
+
+}
