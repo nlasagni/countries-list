47bad94e010a9961c85e948e53debf111ebf3e15
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-08-17 16:59:19 +0200

    Adds actity entry point annotation for hilti

--- app/src/main/java/com/nlasagni/countrieslist/MainActivity.kt
+++ app/src/main/java/com/nlasagni/countrieslist/MainActivity.kt
@@ -33,8 +33,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.nlasagni.countrieslist.ui.theme.CountriesListTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
