3c6ee7698d0c42442547687246446a83b6e5fd96
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-08-18 13:14:19 +0200

    Introduces firts prototype of country list

---
+++ app/src/main/java/com/nlasagni/countrieslist/viewmodel/CountriesListViewModel.kt
@@ -0,0 +1,54 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Nicola Lasagni
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.nlasagni.countrieslist.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nlasagni.countrieslist.data.CountriesListRepository
+import com.nlasagni.countrieslist.data.Country
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+/**
+ * Created by Nicola Lasagni on 17/08/2021.
+ */
+@HiltViewModel
+class CountriesListViewModel @Inject constructor(
+    private val repository: CountriesListRepository
+) : ViewModel() {
+
+    private val _countries = MutableLiveData<List<Country>>()
+
+    // creare ui model con paesi e filtri ed errori eventuali
+
+    init {
+        viewModelScope.launch {
+            _countries.value = repository.fetchAllCountries()
+        }
+    }
+
+}
