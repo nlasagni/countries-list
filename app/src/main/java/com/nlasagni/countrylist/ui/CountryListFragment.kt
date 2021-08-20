0705826a181e089c72f8f25f66f9430945e0a3d9
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-08-20 17:22:24 +0200

    Adds CountryDetailFragment and makes navigation from list to detail available

--- app/src/main/java/com/nlasagni/countrylist/ui/CountryListFragment.kt
+++ app/src/main/java/com/nlasagni/countrylist/ui/CountryListFragment.kt
@@ -30,6 +30,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.nlasagni.countrylist.R
 import com.nlasagni.countrylist.viewmodel.CountryViewModel
@@ -75,7 +76,8 @@ class CountryListFragment : Fragment(), CountryListAdapter.OnItemClickListener {
     }
 
     override fun onItemClick(countryListItem: CountryListItem, position: Int) {
-        //TODO("Not yet implemented")
+        viewModel.onCountryListItemClick(countryListItem)
+        findNavController().navigate(R.id.action_countriesListFragment_to_countryDetailFragment)
     }
 
 
